afdda7a93995b5d958eb6baca0f0e2a01c54f2bb
DanielSalesMelo <269841395+DanielSalesMelo@users.noreply.github.com>
2026-04-17 11:00:39 -0400

    chore(agent): remove target directory from repository

--- synapse-agent/target/debug/deps/synapse_agent-f83fdc30f92dd3f0.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/Synapse/synapse-agent/target/debug/deps/synapse_agent-f83fdc30f92dd3f0.d: src/main.rs
-
-/home/ubuntu/Synapse/synapse-agent/target/debug/deps/synapse_agent-f83fdc30f92dd3f0: src/main.rs
-
-src/main.rs:
--- synapse-agent/target/debug/synapse-agent.d
+++
@@ -1,1 +0,0 @@
-/home/ubuntu/Synapse/synapse-agent/target/debug/synapse-agent: /home/ubuntu/Synapse/synapse-agent/src/main.rs
